8c541dba28df116f2fc3569c63d0e4a023a5dcf9
Hasit Mistry <hasitnm@gmail.com>
2017-05-15 11:00:38 -0700

    Remove omitempty

--- models.go
+++ models.go
@@ -7,49 +7,51 @@ import (
 )
 
 type Health struct {
-	RepositoryURL  string    `json:"repository_url"`
-	RepositoryName string    `json:"repository_name"`
-	Timestamp      time.Time `json:"timestamp"`
-	Indicators     struct {
+	RepositoryOwner    string    `json:"repository_owner"`
+	RepositoryName     string    `json:"repository_name"`
+	RepositoryURL      string    `json:"repository_url"`
+	RepositoryFullName string    `json:"repository_full_name"`
+	Timestamp          time.Time `json:"timestamp"`
+	Indicators         struct {
 		AverageResponseTimes []struct {
-			UserName            string `json:"user_name,omitempty"`
-			AverageResponseTime string `json:"average_response_time,omitempty"`
-		} `json:"average_response_times,omitempty"`
+			UserName            string `json:"user_name"`
+			AverageResponseTime string `json:"average_response_time"`
+		} `json:"average_response_times"`
 		Commits struct {
-			TimeSinceFirstCommit string `json:"time_since_first_commit,omitempty"`
-			TimeSinceLastCommit  string `json:"time_since_last_commit,omitempty"`
+			TimeSinceFirstCommit string `json:"time_since_first_commit"`
+			TimeSinceLastCommit  string `json:"time_since_last_commit"`
 			CommitFrequency      struct {
-				TotalCommits string `json:"total_commits,omitempty"`
-				PerWeek      string `json:"per_week,omitempty"`
-				PerMonth     string `json:"per_month,omitempty"`
-			} `json:"commit_frequency,omitempty"`
-		} `json:"commits,omitempty"`
+				TotalCommits string `json:"total_commits"`
+				PerWeek      string `json:"per_week"`
+				PerMonth     string `json:"per_month"`
+			} `json:"commit_frequency"`
+		} `json:"commits"`
 		Issues struct {
-			TotalIssues        string `json:"total_issues,omitempty"`
-			AverageTimeToClose string `json:"average_time_to_close,omitempty"`
+			TotalIssues        string `json:"total_issues"`
+			AverageTimeToClose string `json:"average_time_to_close"`
 			OpenVsClosed       struct {
-				PerWeek  string `json:"per_week,omitempty"`
-				PerMonth string `json:"per_month,omitempty"`
-			} `json:"open_vs_closed,omitempty"`
-		} `json:"issues,omitempty"`
+				PerWeek  string `json:"per_week"`
+				PerMonth string `json:"per_month"`
+			} `json:"open_vs_closed"`
+		} `json:"issues"`
 		License struct {
-			Exists bool   `json:"exists,omitempty"`
-			URL    string `json:"url,omitempty"`
-			Name   string `json:"name,omitempty"`
-		} `json:"license,omitempty"`
+			Exists bool   `json:"exists"`
+			URL    string `json:"url"`
+			Name   string `json:"name"`
+		} `json:"license"`
 		PullRequests struct {
-			TotalPullRequests  string `json:"total_pull_requests,omitempty"`
-			MergedPullRequests string `json:"merged_pull_requests,omitempty"`
+			TotalPullRequests  string `json:"total_pull_requests"`
+			MergedPullRequests string `json:"merged_pull_requests"`
 			SentVsMerged       struct {
-				PerWeek  string `json:"per_week,omitempty"`
-				PerMonth string `json:"per_month,omitempty"`
-			} `json:"sent_vs_merged,omitempty"`
-		} `json:"pull_requests,omitempty"`
+				PerWeek  string `json:"per_week"`
+				PerMonth string `json:"per_month"`
+			} `json:"sent_vs_merged"`
+		} `json:"pull_requests"`
 		Readme struct {
-			Exists bool   `json:"exists,omitempty"`
-			URL    string `json:"url,omitempty"`
-		} `json:"readme,omitempty"`
-	} `json:"indicators,omitempty"`
+			Exists bool   `json:"exists"`
+			URL    string `json:"url"`
+		} `json:"readme"`
+	} `json:"indicators"`
 }
 
 type Indicator struct {
