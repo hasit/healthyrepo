dfd8e3c7bdeba37ccc93a7777355e860c748428f
Hasit Mistry <hasitnm@gmail.com>
2017-05-15 09:47:16 -0700

    Change GET /health/... to GET /repo/...

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	h := Handler{}
+	err := h.initDB()
+	if err != nil {
+		e.Logger.Panic(err)
+	}
+
+	e.File("/favicon.ico", "images/favicon.png")
+
+	e.GET("/repo/:owner/:repo/health", h.getRepoHealth)
+	e.GET("/indicators", h.getIndicators)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
