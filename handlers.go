d78c844ee727b26e434f3018d2581503ba08152b
Hasit Mistry <hasitnm@gmail.com>
2017-05-15 09:48:01 -0700

    Add handlers for each endpoint

---
+++ handlers.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo"
+)
+
+func (h *Handler) getRepoHealth(c echo.Context) error {
+	db := h.DB.Clone()
+	defer db.Close()
+
+	owner := c.Param("owner")
+	repo := c.Param("repo")
+	queryParams := c.QueryParams()
+
+	health := &Health{
+		RepositoryName: fmt.Sprintf("%s/%s", owner, repo),
+		RepositoryURL:  fmt.Sprintf("github.com/%s/%s", owner, repo),
+		Timestamp:      time.Now(),
+	}
+
+	for queryKey, queryValues := range queryParams {
+		switch queryKey {
+		case "indicators":
+			fmt.Println(queryValues)
+		}
+	}
+
+	return c.JSON(http.StatusOK, health)
+}
+
+func (h *Handler) getIndicators(c echo.Context) error {
+	db := h.DB.Clone()
+	defer db.Close()
+
+	i := db.DB("healthyrepo").C("indicators")
+
+	result := []Indicator{}
+
+	err := i.Find(nil).All(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return c.JSON(http.StatusOK, result)
+}
