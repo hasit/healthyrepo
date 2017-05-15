faf0071ea1546553f0737c283f283240f85cf4d7
Hasit Mistry <hasitnm@gmail.com>
2017-05-15 12:32:24 -0700

    Seperate indicators into their own structs

--- models.go
+++ models.go
@@ -1,49 +1,20 @@
 package main
 
-import (
-	"time"
-
-	"gopkg.in/mgo.v2/bson"
-)
+import "gopkg.in/mgo.v2/bson"
 
 type Health struct {
-	RepositoryOwner    string    `json:"repository_owner"`
-	RepositoryName     string    `json:"repository_name"`
-	RepositoryURL      string    `json:"repository_url"`
-	RepositoryFullName string    `json:"repository_full_name"`
-	Timestamp          time.Time `json:"timestamp"`
+	RepositoryOwner    string `json:"repository_owner"`
+	RepositoryName     string `json:"repository_name"`
+	RepositoryURL      string `json:"repository_url"`
+	RepositoryFullName string `json:"repository_full_name"`
+	Timestamp          string `json:"timestamp"`
 	Indicators         struct {
-		AverageResponseTimes []struct {
-			UserName            string `json:"user_name"`
-			AverageResponseTime string `json:"average_response_time"`
-		} `json:"average_response_times"`
-		Commits struct {
-			TimeSinceFirstCommit string `json:"time_since_first_commit"`
-			TimeSinceLastCommit  string `json:"time_since_last_commit"`
-			CommitFrequency      struct {
-				TotalCommits string `json:"total_commits"`
-				PerWeek      string `json:"per_week"`
-				PerMonth     string `json:"per_month"`
-			} `json:"commit_frequency"`
-		} `json:"commits"`
-		Issues struct {
-			TotalIssues        string `json:"total_issues"`
-			AverageTimeToClose string `json:"average_time_to_close"`
-			OpenVsClosed       struct {
-				PerWeek  string `json:"per_week"`
-				PerMonth string `json:"per_month"`
-			} `json:"open_vs_closed"`
-		} `json:"issues"`
-		License      License `json:"license"`
-		PullRequests struct {
-			TotalPullRequests  string `json:"total_pull_requests"`
-			MergedPullRequests string `json:"merged_pull_requests"`
-			SentVsMerged       struct {
-				PerWeek  string `json:"per_week"`
-				PerMonth string `json:"per_month"`
-			} `json:"sent_vs_merged"`
-		} `json:"pull_requests"`
-		Readme Readme `json:"readme"`
+		AverageResponseTimes []AverageResponseTime `json:"average_response_times"`
+		Commits              Commits               `json:"commits"`
+		Issues               Issues                `json:"issues"`
+		License              License               `json:"license"`
+		PullRequests         PullRequests          `json:"pull_requests"`
+		Readme               Readme                `json:"readme"`
 	} `json:"indicators"`
 }
 
@@ -56,6 +27,38 @@ type License struct {
 	Exists bool   `json:"exists"`
 	URL    string `json:"url"`
 	Name   string `json:"name"`
+}
+
+type PullRequests struct {
+	TotalPullRequests  string `json:"total_pull_requests"`
+	MergedPullRequests string `json:"merged_pull_requests"`
+	SentVsMerged       struct {
+		PerWeek  string `json:"per_week"`
+		PerMonth string `json:"per_month"`
+	} `json:"sent_vs_merged"`
+}
+
+type Issues struct {
+	TotalPullRequests  string `json:"total_pull_requests"`
+	MergedPullRequests string `json:"merged_pull_requests"`
+	SentVsMerged       struct {
+		PerWeek  string `json:"per_week"`
+		PerMonth string `json:"per_month"`
+	} `json:"sent_vs_merged"`
+}
+
+type Commits struct {
+	TotalPullRequests  string `json:"total_pull_requests"`
+	MergedPullRequests string `json:"merged_pull_requests"`
+	SentVsMerged       struct {
+		PerWeek  string `json:"per_week"`
+		PerMonth string `json:"per_month"`
+	} `json:"sent_vs_merged"`
+}
+
+type AverageResponseTime struct {
+	UserName            string `json:"user_name"`
+	AverageResponseTime string `json:"average_response_time"`
 }
 
 type Indicator struct {
