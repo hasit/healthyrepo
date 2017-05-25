42cccfab20ec21a19c41aea460331e7b99389baa
hasit <hasit@gmail.com>
2017-05-24 22:46:28 -0700

    Fix error message when key not found

--- key.go
+++ key.go
@@ -1,11 +1,14 @@
 package main
 
-import "os"
+import (
+	"log"
+	"os"
+)
 
 func getEnv(key string) string {
 	env := os.Getenv(key)
 	if len(env) == 0 {
-		panic("ATLAS_USERNAME environment variable is not set!\n")
+		log.Panicf("%s environment variable is not set!\n", key)
 	}
 	return env
 }
