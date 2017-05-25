61c873e1a4e933ce3218c81fcb38f8d012434885
hasit <hasit@gmail.com>
2017-05-24 22:47:37 -0700

    Seperate util tasks

---
+++ utils.go
@@ -0,0 +1,26 @@
+package main
+
+import "time"
+
+func lastDayOfYear(t time.Time) time.Time {
+	return time.Date(t.Year(), 12, 31, 0, 0, 0, 0, t.Location())
+}
+
+func firstDayOfNextYear(t time.Time) time.Time {
+	return time.Date(t.Year()+1, 1, 1, 0, 0, 0, 0, t.Location())
+}
+
+// a - b in days
+func daysDiff(a, b time.Time) (days int) {
+	cur := b
+	for cur.Year() < a.Year() {
+		// add 1 to count the last day of the year too.
+		days += lastDayOfYear(cur).YearDay() - cur.YearDay() + 1
+		cur = firstDayOfNextYear(cur)
+	}
+	days += a.YearDay() - cur.YearDay()
+	if b.AddDate(0, 0, days).After(a) {
+		days--
+	}
+	return days
+}
