c8001e9a5fc2c36634377905ee841a00dc8375f0
hasit <hasit@gmail.com>
2017-06-01 10:39:29 -0700

    Update routes
    1. Add instrumentation routes
    `GET /_count` - for getting number of goroutines at any moment
    `GET /_stack` - for getting stack trace at any moment
    
    2. `GET /indicators` for getting list or individual indicators
    
    3. `GET /repo` for getting a repo metadata
    
    4. `GET /health` for getting health indicator of repo

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Skipper: middleware.DefaultSkipper,
-		Format:  "{\"time\":\"${time_rfc3339}\", \"remote_ip\":\"${remote_ip}\", \"host\":\"${host}\", \"method\":\"${method}\", \"uri\":\"${uri}\", \"status\":${status}, latency:${latency}, \"latency_human\":\"${latency_human}\", \"bytes_in\":${bytes_in}, \"bytes_out\":${bytes_out}}\n",
+		Format:  "{\"time\":\"${time_rfc3339_nano}\", \"remote_ip\":\"${remote_ip}\", \"host\":\"${host}\", \"method\":\"${method}\", \"uri\":\"${uri}\", \"status\":${status}, latency:${latency}, \"latency_human\":\"${latency_human}\", \"bytes_in\":${bytes_in}, \"bytes_out\":${bytes_out}}\n",
 		Output:  os.Stdout,
 	}))
 	e.Use(middleware.Recover())
@@ -23,13 +23,33 @@ func main() {
 	if err != nil {
 		e.Logger.Panic(err)
 	}
-	e.Logger.Printf("DB handler initiated")
+	e.Logger.Print("DB handler initiated")
 
 	e.File("/favicon.ico", "images/favicon.ico")
 
+	// Instrumentation
+	e.GET("/_count", getGoroutinesCount)
+	e.GET("/_stack", getStackTraceHandler)
+
+	// Indicators
 	e.GET("/indicators", dbh.getIndicators)
-	e.GET("/health/:indicator/github.com/:owner/:repo", dbh.getHealth)
-	e.Logger.Printf("%d routes created: %v", len(e.Routes()), e.Routes())
+	e.GET("/indicators/:indicator", dbh.getIndicator)
+
+	// Repository
+	e.GET("/repo/:owner/:repo", dbh.getRepository)
+
+	// Health
+	e.GET("/repo/:owner/:repo/health/docs", dbh.getRepositoryHealthDocs)
+	e.GET("/repo/:owner/:repo/health/response_times", dbh.getRepositoryHealthResponseTimes)
+
+	// data, err := json.MarshalIndent(e.Routes(), "", "  ")
+	// if err != nil {
+	// 	e.Logger.Fatal(err)
+	// }
+	// ioutil.WriteFile("routes.json", data, 0644)
+	// e.Logger.Print("routes.json file created")
+
+	e.Logger.Printf("%d routes created", len(e.Routes()))
 
 	e.Server.Addr = ":8080"
 
