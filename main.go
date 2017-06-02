66da648111cf95d2a279885ef387af691b77e1f6
hasit <hasit@gmail.com>
2017-06-02 14:30:36 -0700

    Add custrom CORS middleware

--- main.go
+++ main.go
@@ -16,6 +16,10 @@ func main() {
 		Format:  "{\"time\":\"${time_rfc3339_nano}\", \"remote_ip\":\"${remote_ip}\", \"host\":\"${host}\", \"method\":\"${method}\", \"uri\":\"${uri}\", \"status\":${status}, latency:${latency}, \"latency_human\":\"${latency_human}\", \"bytes_in\":${bytes_in}, \"bytes_out\":${bytes_out}}\n",
 		Output:  os.Stdout,
 	}))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderAccessControlAllowOrigin},
+	}))
 	e.Use(middleware.Recover())
 
 	dbh := DBHandler{}
